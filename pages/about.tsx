f21fb5ee9e6c3988a5d1ea332a0593652153d9c0
MadMan24 <mdverdera@yahoo.com>
2022-10-11 23:05:35 +0800

    Applying layout and stylesheet.

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,34 +1,23 @@
-import type { NextPage } from "next";
-import Head from "next/head";
-import Image from "next/image";
-import styles from "@styles/Home.module.css";
-import Navbar from "@components/Navbar";
-import Footer from "@components/Footer";
-import Link from "next/link";
-
-const Home: NextPage = () => {
+const About = () => {
   return (
     <>
       <div>
-        <h1 className={styles.title}>Homepage</h1>
-        <p className={styles.text}>
+        <h1>About</h1>
+        <p>
           Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officiis,
           ipsa? Eligendi corporis sit soluta, ut odit est commodi praesentium
           harum nemo blanditiis, doloremque sequi inventore quasi sunt quas
           quam. Recusandae.
         </p>
-        <p className={styles.text}>
+        <p>
           Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officiis,
           ipsa? Eligendi corporis sit soluta, ut odit est commodi praesentium
           harum nemo blanditiis, doloremque sequi inventore quasi sunt quas
           quam. Recusandae.
         </p>
-        <Link href="/ninjas">
-          <a className={styles.btn}>See Ninjas Listing</a>
-        </Link>
       </div>
     </>
   );
 };
 
-export default Home;
+export default About;
---
+++ pages/ninjas/index.tsx
@@ -0,0 +1,11 @@
+const Ninjas = () => {
+  return (
+    <>
+      <div>
+        <h1>All Ninjas</h1>
+      </div>
+    </>
+  );
+};
+
+export default Ninjas;
